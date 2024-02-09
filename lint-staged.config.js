9341804ce0106c98f587f4bc0db77586eb0597b0
VdustR <vdustr@gmail.com>
2024-02-09 15:35:20 +0800

    build: opt lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,18 @@
-const config = {
-  "**/*.json": "sort-json",
-  "**/package.json": "sort-package-json",
-  "**/*": [
-    "cspell lint --no-must-find-files",
-    "prettier --ignore-unknown --write",
-  ],
-  "**/*.{ts,tsx,js,jsx,cjs}":
-    "eslint --report-unused-disable-directives --fix --max-warnings=0",
-  "packages/app/**/*.{ts,tsx}": () => "pnpm exec tsc -p packages/app",
-};
+import process from "node:process";
+// eslint-disable-next-line dot-notation -- Property 'TYPE' comes from an index signature, so it must be accessed with ['TYPE'].ts(4111)
+const isFormat = process.env["TYPE"] === "format";
+
+const config = isFormat
+  ? {
+      "**/*.json": "sort-json",
+      "**/package.json": "sort-package-json",
+      "**/*": "prettier --ignore-unknown --write",
+      "**/*.{ts,tsx,js,jsx,cjs}":
+        "eslint --report-unused-disable-directives --fix --max-warnings=0",
+    }
+  : {
+      "**/*": "cspell lint --no-must-find-files",
+      "packages/app/**/*.{ts,tsx}": () => "pnpm exec tsc -p packages/app",
+    };
 
 export default config;
