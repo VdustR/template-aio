bc22a12b13bd0c765ae88f7de988ddb3091d63f5
VdustR <vdustr@gmail.com>
2024-08-11 14:27:49 +0800

    build(eslint): include gitignore

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,16 @@
-import { vdustr } from "@repo/eslint";
+import { fileURLToPath } from "node:url";
 
-export default vdustr({
-  react: true,
-});
+import { includeIgnoreFile } from "@eslint/compat";
+import { vdustr } from "@repo/eslint";
+import path from "pathe";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default vdustr(
+  {
+    react: true,
+  },
+  includeIgnoreFile(gitignorePath),
+);
