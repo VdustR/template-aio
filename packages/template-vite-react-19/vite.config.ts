ebb30aa438836528abde4c296724b224a4acc5ad
ViPro (京京) <VdustR@gmail.com>
2025-06-19 14:59:30 +0800

    build(template-vite-react-19): remove redundant assertion (#48)

--- packages/template-vite-react-19/vite.config.ts
+++ packages/template-vite-react-19/vite.config.ts
@@ -1,4 +1,3 @@
-import type { PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -7,7 +6,7 @@ export default defineConfig({
   plugins: [
     tsconfigPaths({
       projects: ["./tsconfig.app.json"],
-    }) as unknown as PluginOption,
+    }),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
